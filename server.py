4aa7482ead6602c508a2f8a945d10d63f2d4a5c6
SamSk <sskurikhin3@gmail.com>
2024-09-17 00:17:26 +0200

    ...

--- server.py
+++ server.py
@@ -106,5 +106,5 @@ def contact_page():
     return render_template('contact_info.html')
 
 if __name__=='__main__':
-    # serve(app,host="0.0.0.0", port=5000)
-    app.run(debug=True)
+    serve(app,host="0.0.0.0", port=5000)
+    # app.run(debug=True)
